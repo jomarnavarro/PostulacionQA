4079621b87d2ca17829bb99c648effe5b853eab8
unknown <jpleal79@gmail.com>
2019-09-15 22:30:05 -0300

    agraga parametros para password y usuario

--- src/test/java/test/MicroPlayTests.java
+++ src/test/java/test/MicroPlayTests.java
@@ -10,8 +10,9 @@ public class MicroPlayTests extends BaseTests {
      public  void testSearchAricle() {
 
         String nombreProducto = dataReader.getData("nombreProducto", 1);
-
-        loginPage.Login("jpleal79@gmail.com", "serrano832"); // login
+        String usuario = dataReader.getData("usuario",1);
+        String password = dataReader.getData("password",1);
+        loginPage.Login(usuario, password); // login
         Assert.assertTrue(loginPage.at());
 
         homePage.buscarEnCajaDeTexto(nombreProducto);      // Buscar articulo en caja de texto.
